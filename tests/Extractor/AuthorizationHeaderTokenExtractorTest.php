02908e8ef18bf9e8f1d77c23abbc3cf48c715da3
Shingo Kumagai <shingo4092@gmail.com>
2016-06-06 13:49:21 +0900

    Use corresponding module in token extractor test

--- tests/Extractor/AuthorizationHeaderTokenExtractorTest.php
+++ tests/Extractor/AuthorizationHeaderTokenExtractorTest.php
@@ -2,7 +2,7 @@
 
 namespace BEAR\JwtAuth\Extractor;
 
-use BEAR\JwtAuth\SymmetricJwtAuthModule;
+use BEAR\JwtAuth\TokenExtractorModule;
 use Ray\Di\Injector;
 
 class AuthorizationHeaderTokenExtractorTest extends \PHPUnit_Framework_TestCase
@@ -13,7 +13,7 @@ class AuthorizationHeaderTokenExtractorTest extends \PHPUnit_Framework_TestCase
     public function shouldExtractToken()
     {
         $_SERVER['HTTP_AUTHORIZATION'] = 'Bearer example_token';
-        $tokenExtractor = (new Injector(new SymmetricJwtAuthModule('HS256', 86400, 'example_secret')))->getInstance(TokenExtractorInterface::class);
+        $tokenExtractor = (new Injector(new TokenExtractorModule()))->getInstance(TokenExtractorInterface::class);
 
         $token = $tokenExtractor->extract();
         $this->assertSame('example_token', $token);
@@ -25,7 +25,7 @@ class AuthorizationHeaderTokenExtractorTest extends \PHPUnit_Framework_TestCase
     public function shouldReturnNullCharacter()
     {
         $_SERVER['HTTP_AUTHORIZATION'] = 'Bear example_token';
-        $tokenExtractor = (new Injector(new SymmetricJwtAuthModule('HS256', 86400, 'example_secret')))->getInstance(TokenExtractorInterface::class);
+        $tokenExtractor = (new Injector(new TokenExtractorModule()))->getInstance(TokenExtractorInterface::class);
 
         $token = $tokenExtractor->extract();
         $this->assertSame('', $token);
--- tests/Extractor/QueryParameterTokenExtractorTest.php
+++ tests/Extractor/QueryParameterTokenExtractorTest.php
@@ -2,7 +2,7 @@
 
 namespace BEAR\JwtAuth\Extractor;
 
-use BEAR\JwtAuth\SymmetricJwtAuthModule;
+use BEAR\JwtAuth\TokenExtractorModule;
 use Ray\Di\Injector;
 
 class QueryParameterTokenExtractorTest extends \PHPUnit_Framework_TestCase
@@ -13,7 +13,7 @@ class QueryParameterTokenExtractorTest extends \PHPUnit_Framework_TestCase
     public function shouldExtractToken()
     {
         $_GET['token'] = 'example_token';
-        $tokenExtractor = (new Injector(new SymmetricJwtAuthModule('HS256', 86400, 'example_secret')))->getInstance(TokenExtractorInterface::class, 'query');
+        $tokenExtractor = (new Injector(new TokenExtractorModule()))->getInstance(TokenExtractorInterface::class, 'query');
 
         $token = $tokenExtractor->extract();
         $this->assertSame('example_token', $token);
@@ -25,7 +25,7 @@ class QueryParameterTokenExtractorTest extends \PHPUnit_Framework_TestCase
     public function shouldReturnNullCharacter()
     {
         $_GET['invalid_key'] = 'example_token';
-        $tokenExtractor = (new Injector(new SymmetricJwtAuthModule('HS256', 86400, 'example_secret')))->getInstance(TokenExtractorInterface::class, 'query');
+        $tokenExtractor = (new Injector(new TokenExtractorModule()))->getInstance(TokenExtractorInterface::class, 'query');
 
         $token = $tokenExtractor->extract();
         $this->assertSame('', $token);
